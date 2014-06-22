60a0edab0382ba72df591ef2a8cea9cddb71512e
beaulac <antoine.beaulac@gmail.com>
2014-06-22 14:02:11 -0400

    Add audioplayer BASIC FUNCTIONALITY

--- app/scripts/playerSetup.js
+++ app/scripts/playerSetup.js
@@ -1,9 +1,13 @@
+/**
+ * Created by abl on 22/06/14.
+ */
+
 $(document).ready(function(){
     $("#jquery_jplayer_1").jPlayer({
         ready: function () {
             $(this).jPlayer("setMedia", {
                 title: "Funeral March",
-                mp3: "/audio/01_Funeral_March.mp3"
+                mp3: "../audio/01_Funeral_March.mp3"
             });
         },
         swfPath: "/js",
