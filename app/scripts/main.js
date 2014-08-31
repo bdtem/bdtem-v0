ad6713c52ecbc5e39533b0cffc0d4341fef24f46
beaulac <antoine.beaulac@gmail.com>
2014-08-31 16:05:44 -0400

    remove jplayer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-    $("#jquery_jplayer_1").jPlayer({
-        ready: function () {
-            $(this).jPlayer("setMedia", {
-                title: "Funeral March",
-                mp3: "/audio/01_Funeral_March.mp3"
-            });
-        },
-        swfPath: "/js",
-        supplied: "mp3"
-    });
 
     $("#metainfo a").popover({
         placement : 'top'
