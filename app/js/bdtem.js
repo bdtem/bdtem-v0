7479f48d9373476f7803b4928c859bcc9718452a
Rick_Jasper <tankenginemusic@gmail.com>
2014-10-27 21:02:24 -0400

    Added Metadata Info

--- app/js/bdtem.js
+++ app/js/bdtem.js
@@ -79,7 +79,6 @@ bdtem.controller('PlaylistCtrl', function ($scope, $filter, hotkeys, $sce) {
     ];
 
     $scope.titles = [
-        "",
         "Funeral March",
         "Hesitating Sun",
         "Future Is Bleaker",
@@ -112,7 +111,7 @@ bdtem.controller('PlaylistCtrl', function ($scope, $filter, hotkeys, $sce) {
         },
         {
             title: "Future is Bleaker",
-            description: "Picture him in his rooms, where he is surrounded by works unfinished. A noodling doodle on a page reminding him of past failures, but taunting him with efforts expected." +
+            description: "Picture him in his rooms, where he is surrounded by works unfinished. A noodling doodle on a page reminding him of past failures, but taunting him with efforts expected. " +
                 "He is agitated. And his mind is twisting. And he is thinking upon some vision of the future.",
             catalog: "Gr. 0, No. 3"
         },
@@ -140,15 +139,15 @@ bdtem.controller('PlaylistCtrl', function ($scope, $filter, hotkeys, $sce) {
                 "The grass grown tall. The firing flies flashing. " +
                 "The white moonlight mixing with the distant yellow fires of electric bulbs. " +
                 "He sees a place of far-off interest, emanating also sounds and smells. " +
-                "It is a Circus. " +
+                "It is a Circus. <br/><br/>" +
                 "Fresh from his waiting turmoil, he stumbles toward this beacon in the night, having no other place to go.",
             catalog: "Cr. 0, No. 1"
         },
         {
             title: "The Debate",
-            description: "After being overwhelmed by experiences, laying one lesson on top of another, he has arrived at a brilliant idea, so he seeks an audience with Circus management. " +
-                "He hopes to convince the manager to let him stage a performance. " +
-                "Right away and with the support of the whole Circus. " +
+            description: "After being overwhelmed by experiences, laying one lesson on top of another, he has arrived at a brilliant idea; so he seeks an audience with Circus management. " +
+                "<br/><br/>He hopes to convince the manager to let him stage a performance " +
+                "(right away and with the support of the whole Circus). " +
                 "He could shake off the cobwebs with the sound of applause." +
                 "<br/><br/>A \"Good evening\" and \"thank you\" should do. " +
                 "If he could have but 5 minutes to explain... perhaps longer.  " +
@@ -157,14 +156,100 @@ bdtem.controller('PlaylistCtrl', function ($scope, $filter, hotkeys, $sce) {
         },
         {
             title: "Working at First",
-            description: "After being overwhelmed by experiences, laying one lesson on top of another, he has arrived at a brilliant idea, so he seeks an audience with Circus management. " +
-                "He hopes to convince the manager to let him stage a performance. " +
-                "Right away and with the support of the whole Circus. " +
-                "He could shake off the cobwebs with the sound of applause." +
-                "<br/><br/>A \"Good evening\" and \"thank you\" should do. " +
-                "If he could have but 5 minutes to explain... perhaps longer.  " +
-                "Complex ideas require complex explanations.",
+            description: "The story he tells begins something like this: <br/><br/> " +
+           "\"I started, a long time ago, working on a project that seemed to me fun and perhaps a little challenging. " +
+               " Or at least, it seemed like it would be easy.  That's why I decided to do it.  From the first, " +
+            "I felt that I could see the results in their complete form.  Or, it was more like feeling a sensation of " +
+                "easily obtainable completion. <br/> " +
+            "I used this clarity of vision to guide me forward.  How hard could an easy something be anyway?\" " +
+            "<br/><br/>He attempts to clarify by saying this: <br/><br/> " +
+        "\"I worked in an office.  I was given access to what seemed like limitless resources.  My inspiration was " +
+            "given to me in the form of instructions and tasks.  I worked diligently on many of these tasks, " +
+            "and patiently on some others.  I set out on a path to accomplishment at a time when I was not afraid to " +
+            "fail.\" ",
             catalog: "Cr. 0, No. 3"
+        },
+        {
+            title: "Tried to Be",
+            description: "As he sits and makes explanations, he remembers something he once read; " +
+                "or perhaps he is thinking it into existence at this very moment:<br/><br/>" +
+
+            "\"I can recall the events of my death <br/>" +
+                "as though they were the memories of another man <br/>" +
+            "the biting braid which snuffed my breath <br/>" +
+            "the bottles of fluids and powders in cans\"" +
+
+            "<br/><br/>And this is where the story becomes strange and full of other stories.",
+            catalogue: "Cr. 0, No. 4"
+        },
+        {
+            title: "The Basement",
+            description: "\"When the brain says goodbye, as far as I can tell, God seems to give you a secret.\" <br/><br/>" +
+        "\"Do you believe in God?\" He is asked. <br/><br/>" +
+            "Taking a moment, as though caught off guard by such a question, he replies, \"No, I don't think so.\"",
+            catalogue: "Cr. 0, No. 5"
+        },
+        {
+            title: "One Level at a Time",
+            description: "A body stacked upon a body <br/>" +
+            "repeated into heights. <br/>" +
+            "All those brains, <br/>" +
+        "stacks of wisdom <br/>" +
+        "and naive judgements <br/>" +
+        "and prejudices <br/>" +
+        "and dead thoughts <br/><br/>" +
+
+        "If I were stacked that way, <br/>" +
+        "part of the pile, <br/>" +
+        "between two others <br/>" +
+        "or on top <br/>" +
+        "or between this one and the ground, <br/>" +
+        "I would be as meaningless <br/>" +
+        "as anything else <br/>" +
+    "that has been stacked <br/>" +
+    "and forgotten.",
+            catalogue: "Cr. 0, No. 6"
+        },
+        {
+            title: "Every Sound in a Row",
+            description: "ONE:<br/>" +
+            "I broke my back <br/>" +
+            "and my mind, <br/>" +
+            "this time beyond repair. <br/>" +
+            "I drew into a thought that <br/>" +
+            "bit with poison, <br/>" +
+            "and a noisome flesh remained. <br/><br/>" +
+
+           "TWO: <br/>" +
+    "Waking in a place unfamiliar to me, <br/>" +
+        "I stumbled down a flight of steps <br/>" +
+    "and kept stumbling down 9 more <br/>" +
+    "until, at length, I reached <br/>" +
+    "the bottom, where I was also <br/>" +
+    "not supposed to be. <br/><br/>" +
+
+        "THREE: <br/>" +
+    "Having time to think <br/>" +
+    "is the perfect sort of thing. <br/>" +
+        "Taking time to rest <br/>" +
+    "is prescription at its best. <br/>" +
+        "A mind can’t be lost <br/>" +
+    "if you grab it with both hands. <br/><br/>" +
+
+       "Memory Four= My Time Left.",
+            catalogue: "Cr. 0, No. 7"
+        },
+        {
+            title: "The End",
+            description: "FOUR: <br/>" +
+            "A mind let go <br/>" +
+            "is a mind unknown, <br/>" +
+            "is a mind let grown. <br/>" +
+            "Collapse the tired, buckling knees, <br/>" +
+        "release the dreaming from your sleep. <br/>" +
+            "Never stop, I suppose. <br/>" +
+            "Even though no one is listening.",
+            catalogue: "Cr. 0, No. 8"
         }
     ];
 
