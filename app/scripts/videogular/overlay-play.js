1ce1da01a69163c5eed181ce69fb41e35cde9820
abl <antoine.beaulac@gmail.com>
2014-11-14 21:15:54 -0500

    Fix a bunch of things, get overlay-play into the app so we can change the template.

---
+++ app/scripts/videogular/overlay-play.js
@@ -0,0 +1,80 @@
+/**
+ * @license Videogular v0.7.0 http://videogular.com
+ * Two Fucking Developers http://twofuckingdevelopers.com
+ * License: MIT
+ */
+/**
+ * @ngdoc directive
+ * @name com.2fdevs.videogular.plugins.overlayplay:vgOverlayPlay
+ * @restrict E
+ * @description
+ * Shows a big play button centered when player is paused or stopped.
+ *
+ * ```html
+ * <videogular vg-theme="config.theme.url" vg-autoplay="config.autoPlay">
+ *    <vg-video vg-src="sources"></vg-video>
+ *
+ *    <vg-overlay-play></vg-overlay-play>
+ * </videogular>
+ * ```
+ *
+ * BDTEM EDIT :D
+ *
+ */
+"use strict";
+angular.module("com.2fdevs.videogular.plugins.overlayplay", [])
+    .directive(
+    "vgOverlayPlay",
+    ["VG_STATES", function (VG_STATES) {
+        return {
+            restrict: "E",
+            require: "^videogular",
+            template: "<div class='overlayPlayContainer' ng-click='customClickOverlayPlay()'>" +
+                "<div class='iconButton' ng-class='overlayPlayIcon'></div>" +
+                "</div>",
+            link: function (scope, elem, attr, API) {
+                function onComplete(target, params) {
+                    scope.overlayPlayIcon = {bdtemPlay: true};
+                }
+
+                function onPlay(target, params) {
+                    scope.overlayPlayIcon = {};
+                }
+
+                function onChangeState(newState) {
+                    switch (newState) {
+                        case VG_STATES.PLAY:
+                            scope.overlayPlayIcon = {};
+                            break;
+
+                        case VG_STATES.PAUSE:
+                            scope.overlayPlayIcon = {bdtemPlay: true};
+                            break;
+
+                        case VG_STATES.STOP:
+                            scope.overlayPlayIcon = {};
+                            break;
+                    }
+                }
+
+                scope.onClickOverlayPlay = function onClickOverlayPlay(event) {
+                    API.playPause();
+                };
+
+                scope.overlayPlayIcon = {};
+
+                scope.$watch(
+                    function () {
+                        return API.currentState;
+                    },
+                    function (newVal, oldVal) {
+                        if (newVal != oldVal) {
+                            onChangeState(newVal);
+                        }
+                    }
+                );
+            }
+        }
+    }
+    ]);
+
