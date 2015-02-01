6eb339efed11c3243955150c4b95a59003dd8d75
abl <antoine.beaulac@gmail.com>
2015-02-01 12:54:16 -0500

    Drawers work in progress using Sidr

--- app/js/jquery_fns.js
+++ app/js/jquery_fns.js
@@ -63,7 +63,30 @@ var positionTheVolumeBar = function () {
 $(document).ready(function () {
     stretchTheText();
     positionTheVolumeBar();
+
 });
+
+$(document).ready(function() {
+    $('#simple-menu').sidr({
+        name: 'sidr',
+        speed: 200,
+        side: 'left',
+        source: null,
+        renaming: true,
+        body: 'body'
+
+    });
+});
+
+$(document).ready(function() {
+    $('.sub-menu-sidr').hide();
+
+    $("#sidr li:has(ul)").click(function(){
+
+        $("ul",this).toggle('fast');
+    });
+});
+
 
 $(window).resize(function () {
     positionTheVolumeBar();
