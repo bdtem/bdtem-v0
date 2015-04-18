db4fb06311c9bdb15593ca62362217105eb881d9
antoine beaulac <antoine.beaulac@gmail.com>
2015-04-18 16:50:02 -0400

    Add 0-padding to track numbers

--- app/js/filters.js
+++ app/js/filters.js
@@ -11,6 +11,15 @@ angular.module('bdtemFilters', [])
     return function (value) {
         return value ? value : 0;
     };
+}).filter('padWithZero', function () {
+    return function (value) {
+        if(typeof value === "number") {
+            if(value < 10) {
+             return "0" + value;
+            }
+        }
+        return value;
+    };
 }).filter('trustAsHtml', ['$sce', function ($sce) {
         return function (val) {
             return $sce.trustAsHtml(val);
