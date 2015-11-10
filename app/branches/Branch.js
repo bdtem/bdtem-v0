d14e1635d7c550028dc9113e77ca5d953689b652
antoine beaulac <antoine.beaulac@gmail.com>
2015-11-10 13:24:59 -0500

    Split js into multiple files. BranchGroup and Branch need to be merged into a single type that recursively contains more Branches

---
+++ app/branches/Branch.js
@@ -0,0 +1,73 @@
+function Branch(svgGroup, startX, startY, endX, endY, text, branchType) {
+  this.svgGroup = svgGroup;
+  this.startX = startX;
+  this.startY = startY;
+  this.endX = endX;
+
+  if (branchType) {
+    this.updateAnimation = branchType.updateAnimation;
+  }
+
+  this.length = endX - startX;
+
+  this.branchLine = this.buildBranchLine(startX, startY, startX, startY);
+
+  this.textNode = text && buildTextNode(text, startX, startY, false, true).attr({opacity: 0});
+}
+
+Branch.prototype.destroyBranch = function () {
+  this.animateTrunk(this.branchLine.attr('x2'), this.startX, this.getRemovalAnimation());
+};
+
+Branch.prototype.animateIn = function () {
+  this.textNode && this.textNode.attr({opacity: 1});
+  this.animateTrunk(this.startX, this.endX);
+};
+
+Branch.prototype.buildBranchLine = function (x1, y1, x2, y2) {
+  var line = this.svgGroup.line(x1, y1, x2, y2);
+  line.attr({stroke: STROKE_COLOR, 'stroke-width': 2});
+  return line;
+};
+
+Branch.prototype.getRemovalAnimation = function () {
+  var self = this;
+  return function () {
+    self.branchLine.remove();
+
+    if (self.textNode && self.textNode.attr('opacity') > 0) {
+      self.textNode.animate(
+        {opacity: 0},
+        250,
+        function () {
+          self.textNode.remove()
+        }
+      );
+    }
+  }
+};
+
+Branch.prototype.animateTrunk = function (from, to, callback) {
+
+  Snap.animate(
+    from,
+    to,
+    this.updateAnimation(),
+    250,
+    mina.easeout,
+    callback
+  );
+};
+
+Branch.prototype.updateAnimation = function () {
+  var self = this;
+  return function (value) {
+
+    var txt = self.textNode;
+    if (txt) {
+      txt.attr({x: self.length < 0 ? value - (txt.getBBox().width + 1) : value});
+    }
+
+    self.branchLine.attr({x2: value});
+  };
+};
---
+++ app/branches/BranchGroup.js
@@ -0,0 +1,158 @@
+function BranchGroup(svgGroup,
+                     trunkLength,
+                     numberOfBranches,
+                     branchLength,
+                     animationDuration,
+                     branchParameters) {
+
+  this.branchParameters = branchParameters;
+  this.svgGroup = svgGroup;
+  this.animationDuration = animationDuration || 1000;
+  this.numberOfBranches = numberOfBranches || 1;
+
+  this.trunkLength = trunkLength || 150;
+  this.branchLength = branchLength || 75;
+
+  var bBox = svgGroup.getBBox();
+  this.startY = bBox.y2;
+  this.startX = bBox.cx;
+  this.endX = this.getEndX();
+  this.endY = this.getEndY();
+
+  this.yDistance = (this.endY - this.startY);
+
+  this.branches = [];
+  this.buildBranchTimeOffsetsAndPoints();
+  this.pendingBranchAnimations = new Array(numberOfBranches);
+}
+
+BranchGroup.prototype.getEndY = function () {
+  //TODO GET THIS FROM THE BRANCH TYPE
+  return this.startY + this.trunkLength;
+};
+
+BranchGroup.prototype.getEndX = function () {
+  //TODO GET THIS FROM THE BRANCH TYPE
+  return this.startX;
+};
+
+BranchGroup.prototype.destroySubBranches = function () {
+  this.pendingBranchAnimations.forEach(clearTimeout);
+  this.branches.forEach(function (elem) {
+    elem.destroyBranch();
+  });
+
+  this.branches = [];
+  this.pendingBranchAnimations = [];
+
+  //Pre-rebuild branches for next run:
+  this.buildBranchTimeOffsetsAndPoints();
+};
+
+BranchGroup.prototype.stopAnimation = function () {
+  if (this.animationInProgress) {
+    this.animationInProgress.stop();
+  }
+};
+
+BranchGroup.prototype.destroyBranch = function () {
+  this.destroySubBranches();
+  var trunk = this.trunk;
+
+  this.stopAnimation();
+
+  trunk.stop();
+  trunk.animate(
+    {opacity: 0},
+    500,
+    removeAfterAnimation(trunk)
+  );
+};
+
+function removeAfterAnimation(node) {
+  return function () {
+    node.remove()
+  };
+}
+
+BranchGroup.prototype.buildBranchTimeOffsetsAndPoints = function () {
+  var self = this;
+
+  if (this.branchParameters) {
+
+    this.branches = this.branchParameters.branches;
+    this.branches.forEach(function (branch) {
+      self.svgGroup.append(branch);
+    });
+
+  } else {
+
+    var numberOfBranches = this.numberOfBranches;
+    for (var i = 1; i <= numberOfBranches; i++) {
+      var branchPoint = this.startY + (i / numberOfBranches) * this.yDistance;
+      this.addBranch(this.startX, branchPoint);
+    }
+
+  }
+
+  this.branchTimeOffsets = new Array(numberOfBranches);
+  this.branches.forEach(function (elem, index) {
+    self.branchTimeOffsets[index] = (index + 1) / numberOfBranches * self.animationDuration;
+  })
+};
+
+BranchGroup.prototype.drawTrunkWithBranchesTo = function (x, y) {
+  this.trunk = new Branch(this.svgGroup, this.startX, this.startY, x || this.startX, y || this.startY, '', BRANCH_TYPE.VERTICAL).branchLine;
+
+  this.animateTrunk();
+
+  return this;
+};
+
+BranchGroup.prototype.animateTrunk = function () {
+  this.animationInProgress = Snap.animate(
+    this.startY,
+    this.endY,
+    this.updateVertical(this),
+    this.animationDuration
+  );
+};
+
+BranchGroup.prototype.updateVertical = function (branchGroup) {
+  var branchAnimations = this.pendingBranchAnimations;
+
+  this.branchTimeOffsets.forEach(function (timeOffset, index) {
+    var asyncAnimation = setTimeout(function () {
+      var branch = branchGroup.branches[index];
+      branch.animateIn();
+    }, timeOffset);
+
+    branchAnimations.push(asyncAnimation)
+  });
+
+  return function updateLine(value) {
+    branchGroup.trunk.attr({y2: value});
+  }
+};
+
+BranchGroup.prototype.addBranch = function (startX, branchY) {
+  startX = startX || this.startX;
+
+  var branch = new Branch(
+    this.svgGroup,
+    startX,
+    branchY,
+    Math.random() >= 0.5 ? startX - this.branchLength : startX + this.branchLength,
+    branchY,
+    this.getBranchText(),
+    BRANCH_TYPE.HORIZONTAL
+  );
+
+  this.branches.push(branch);
+  this.svgGroup.append(branch.branchLine);
+};
+
+
+BranchGroup.prototype.getBranchText = function () {
+  return randomColor();
+};
